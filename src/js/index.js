d5f82aae3b8ee134cba6832cf08ed10b9ab93d50
ajiir <ajiir2247@gmail.com>
2021-01-12 14:12:44 +0800

    Huudaslalt bolon search model implement hiine

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
+import Recipe from "./model/Recipe";
 
 /**
  * Web app төлөв
@@ -29,7 +30,7 @@ const controlSearch = async () => {
 
     //   5. Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
     clearLoader();
-    if (state.search.result === undefined) alert("Хайлт илэрцгүй....");
+    if (state.search.result === undefined) alert("Хайлт илэрцгүй...");
     else searchView.renderRecipes(state.search.result);
   }
 };
@@ -38,3 +39,16 @@ elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
+
+elements.pageButtons.addEventListener("click", (e) => {
+  const btn = e.target.closest(".btn-inline");
+
+  if (btn) {
+    const gotoPageNumber = parseInt(btn.dataset.goto, 10);
+    searchView.clearSearchResult();
+    searchView.renderRecipes(state.search.result, gotoPageNumber);
+  }
+});
+
+const r = new Recipe(47746);
+r.getRecipe();
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -34,8 +34,49 @@ export const clearSearchQuery = () => {
 
 export const clearSearchResult = () => {
   elements.searchResultList.innerHTML = "";
+  elements.pageButtons.innerHTML = "";
 };
 export const getInput = () => elements.searchInput.value;
-export const renderRecipes = (recipes) => {
-  recipes.forEach(renderRecipe);
+
+export const renderRecipes = (recipes, currentPage = 1, resPerPage = 10) => {
+  // Хайлтын үр дүнг хуудаслаж үзүүлэх эхний хэсэг
+  // page = 2, start = 10, end = 20
+  const start = (currentPage - 1) * resPerPage;
+  const end = currentPage * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // Хуудаслалтын товчуудыг гаргаж ирэх
+  const totalPages = Math.ceil(recipes.length / resPerPage);
+  renderButtons(currentPage, totalPages);
+};
+
+// type ====> "prev", "next"
+const createButton = (
+  page,
+  type,
+  direction
+) => `<button class="btn-inline results__btn--${type}" data-goto=${page}>
+<span>Хуудас ${page}</span>
+<svg class="search__icon">
+  <use href="img/icons.svg#icon-triangle-${direction}"></use>
+</svg>
+</button>`;
+
+const renderButtons = (currentPage, totalPages) => {
+  let buttonHTML;
+
+  if (currentPage === 1 && totalPages > 1) {
+    // 1-Р хуудсан дээр байна. 2-р хуудас гэдэг товчийг гарга
+    buttonHTML = createButton(2, "next", "right");
+  } else if (currentPage < totalPages) {
+    // Өмнөх болон дараачийн хуудасруу шилжих товчуудыг үзүүл.
+    buttonHTML = createButton(currentPage - 1, "prev", "left");
+    buttonHTML += createButton(currentPage + 1, "next", "tight");
+  } else if (currentPage === totalPages) {
+    // Хамгийн сүүлийн хуудас дээр байна. Өмнөхрүүгээ шилжүүлэх товчийг үзүүлнэ.
+    buttonHTML = createButton(currentPage - 1, "prev", "left");
+  }
+
+  elements.pageButtons.insertAdjacentHTML("afterbegin", buttonHTML);
 };
